676413815b458a1f8b0661d03b5d4fa2e5cabdcd
ax-sylvester <ax-sylvester.hommuk@khk.ee>
2022-10-26 09:36:55 +0300

    Fetched city weather data from API

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // Basic
+
+document.addEventListener( 'DOMContentLoaded', cityWeather)
+
+function weatherDataFetch(city){
+    var key = '7122c5b1245fc8d20e6d2ea2793ee2e4';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then(function (resp) {
+            return resp.json()
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+        .
+        catch(function () {
+
+        });
+}
+
+function cityWeather (e){
+    weatherDataFetch('Tallinn');
+}
