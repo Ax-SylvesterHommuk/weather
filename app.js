819aff48e54fee88405494f2fb907cc89aa26f49
ax-sylvester <ax-sylvester.hommuk@khk.ee>
2022-10-26 09:41:28 +0300

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // Basic
-
-document.addEventListener( 'DOMContentLoaded', cityWeather)
+document.addEventListener('DOMContentLoaded', cityWeather)
 
 function weatherDataFetch(city){
     var key = '7122c5b1245fc8d20e6d2ea2793ee2e4';
@@ -8,15 +7,26 @@ function weatherDataFetch(city){
         .then(function (resp) {
             return resp.json()
         })
-        .then(function(data) {
+        .then(function (data) {
             console.log(data);
+            drawWeather(data);
         })
-        .
-        catch(function () {
-
+        .catch(function (){
         });
 }
 
-function cityWeather (e){
-    weatherDataFetch('Tallinn');
+
+// getting the city
+function cityWeather(e){
+    weatherDataFetch('Tartu');
+}
+
+// displaying the data
+function drawWeather(data){
+    var celcius = Math.round(parseFloat(data.main.temp) - 273.15);
+    var description = data.weather[0].description;
+
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML = celcius + '&deg';
+    document.querySelector('#location').innerHTML = data.name;
 }
